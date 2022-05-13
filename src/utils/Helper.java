883637ad739f442870bd052c76e24c91e127f4e5
Abdelrahman Fathy <abdof1723@gmail.com>
2022-05-13 22:21:43 +0200

    Implement setBit and clearBit

--- src/utils/Helper.java
+++ src/utils/Helper.java
@@ -6,18 +6,16 @@ public class Helper {
 	public static final int POS_EXTENSION_MASK = ((1 << 8) - 1);
 	
 	public static int signExtend(int data) {
-		if ((1 << 8 & data) != 0)
+		if ((1 << 7 & data) != 0)
 			return data | NEG_EXTENSION_MASK;
 		return data & POS_EXTENSION_MASK;
 	}
 
-	public static int setBit(int sREG, int i) {
-		// TODO Auto-generated method stub
-		return 0;
+	public static int setBit(int SREG, int i) {
+		return SREG | 1<<i;
 	}
 
-	public static int clearBit(int sREG, int i) {
-		// TODO Auto-generated method stub
-		return 0;
+	public static int clearBit(int SREG, int i) {
+		return SREG & (-1-(1<<i));
 	}
 }
