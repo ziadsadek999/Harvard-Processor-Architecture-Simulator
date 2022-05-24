e89a520623ca8ae8f94742f0010b976b0224ec35
AyaFayed <79098945+AyaFayed@users.noreply.github.com>
2022-05-24 02:24:26 +0200

    Dark Mode

--- src/views/MainFrame.java
+++ src/views/MainFrame.java
@@ -23,19 +23,22 @@ public class MainFrame extends JFrame implements ActionListener {
 	public MainFrame() {
 		codeRead = false;
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setTitle("Neptune Simulator");
 		setMinimumSize(new Dimension(500, 500));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		mainPanel = new JPanel();
 		mainPanel.setLayout(new GridLayout(1, 2));
-		code = new TextArea();
-		code.setFont(new Font("Berlin Sans FB Demi", 2, 40));
+		code = new TextArea("Enter Your Code Here");
+		code.setFont(new Font("Calisto MT", 0, 22));
+		code.setBackground(new Color(16, 16, 24));
+		code.setForeground(new Color(215, 65, 167));
 		mainPanel.add(code);
 		JPanel right = new JPanel();
 		right.setLayout(new BoxLayout(right, BoxLayout.Y_AXIS));
 		log = new TextArea(20, 200);
 		log.setEditable(false);
-		log.setBackground(Color.black);
-		log.setForeground(Color.RED);
+		log.setBackground(new Color(3, 3, 10));
+		log.setForeground(Color.GREEN);
 		right.add(log);
 		nextLine = new JButton("Next Line");
 		runAll = new JButton("Run ALL");
@@ -43,13 +46,20 @@ public class MainFrame extends JFrame implements ActionListener {
 		nextLine.addActionListener(this);
 		runAll.addActionListener(this);
 		reset.addActionListener(this);
+		nextLine.setBackground(new Color(3, 3, 10));
+		runAll.setBackground(new Color(3, 3, 10));
+		reset.setBackground(new Color(3, 3, 10));
+		nextLine.setForeground(new Color(108, 207, 246));
+		runAll.setForeground(Color.GREEN);
+		reset.setForeground(Color.RED);
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.add(nextLine);
 		buttonPanel.add(runAll);
 		buttonPanel.add(reset);
 		right.add(buttonPanel);
 		TextArea title = new TextArea("REGISTERS", 1, 20, TextArea.SCROLLBARS_NONE);
-
+        title.setBackground(new Color(3, 3, 10));
+        title.setForeground(new Color(100, 100, 100));
 		title.setEditable(false);
 		right.add(title);
 		JPanel rightBottom = new JPanel();
@@ -58,6 +68,7 @@ public class MainFrame extends JFrame implements ActionListener {
 		for (int i = 0; i < 16; i++) {
 			JPanel quadrable = new JPanel();
 			quadrable.setLayout(new GridLayout(1, 4));
+			quadrable.setBackground(new Color(3, 3, 10));
 			registers[i] = new TextArea("", 1, 20, TextArea.SCROLLBARS_NONE);
 			registers[i].setText("R" + i + ": 0b00000000/0");
 			registers[i + 16] = new TextArea("", 1, 20, TextArea.SCROLLBARS_NONE);
@@ -70,6 +81,14 @@ public class MainFrame extends JFrame implements ActionListener {
 			registers[i + 32].setText("R" + (i + 32) + ": 0b00000000/0");
 			registers[i + 48].setEditable(false);
 			registers[i + 32].setEditable(false);
+			registers[i].setBackground(new Color(3, 3, 10));
+		    registers[i + 16].setBackground(new Color(3, 3, 10));
+			registers[i + 32].setBackground(new Color(3, 3, 10));
+			registers[i + 48].setBackground(new Color(3, 3, 10));
+			registers[i].setForeground(new Color(100, 100, 100));
+		    registers[i + 16].setForeground(new Color(100, 100, 100));
+			registers[i + 32].setForeground(new Color(100, 100, 100));
+			registers[i + 48].setForeground(new Color(100, 100, 100));	
 			quadrable.add(registers[i]);
 			quadrable.add(registers[i + 16]);
 			quadrable.add(registers[i + 32]);
